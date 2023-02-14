d56e66a00b2235e8c351c8ce845d0fc1a957fad4
Sergei R <rse017@gmail.com>
2023-02-15 01:52:58 +0300

    feat: test solved

--- src/index.js
+++ src/index.js
@@ -38,9 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i = i + 10) {
+        let el = expr.slice(i, i + 10);
+        if (el === '**********') {
+            result += ' '; 
+            continue;
+        } 
+        let letter = '';
+        for (let k = 0; k < el.length; k = k + 2) {
+            let num = el.slice(k, k+2);     
+            if (num === '10'){
+                letter += '.';
+            } else if (num === '11') {
+                letter += '-';
+            }
+        }
+        result += MORSE_TABLE[letter];
+    }
+    return result;
 }
-
 module.exports = {
     decode
 }
